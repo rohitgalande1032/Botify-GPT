d4786c30756f662d5cc0d3c46b4e612561608bc1
rohitgalande1032 <rohitg3234@gmail.com>
2024-10-06 18:33:43 +0530

    Create pages and Logo ccomponent

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import { createTheme, ThemeProvider } from '@mui/material'
+import { BrowserRouter } from 'react-router-dom'
+const theme = createTheme({
+  typography: {
+    fontFamily: "ROBOTO slab, serif",
+    allVariants: { color: "white" }
+  }
+})
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
