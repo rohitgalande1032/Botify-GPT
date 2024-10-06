feb96c9311301d4a7beb2fe23d8d668d285533b6
rohitgalande1032 <rohitg3234@gmail.com>
2024-10-07 03:30:31 +0530

    Add Open AI API ang Chat Logic

---
+++ backend/src/controllers/chat-controllers.ts
@@ -0,0 +1,32 @@
+import { ChatCompletionRequestMessage, OpenAIApi } from "openai"
+import { configureOpenAI } from "../config/openai-config.js"
+import User from "../models/User.js"
+
+export const generateChatCompletion = async (req, res, next) => {
+    const { message } = req.body
+    try {
+        const user = await User.findById(res.locals.jwtData.id)
+        if (!user) return res.status(401).json({ message: "User not registered OR token malfunctioned" })
+        //grab chats of user
+
+        const chats = user.chats.map(({ role, content }) => ({ role, content })) as ChatCompletionRequestMessage[]
+        chats.push({ content: message, role: "user" })
+        user.chats.push({ content: message, role: "user" })
+
+        //send all chats with new one to openAI API
+        const config = configureOpenAI()
+        const openai = new OpenAIApi(config)
+        const chatResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: chats,
+        })
+        user.chats.push(chatResponse.data.choices[0].message)
+        await user.save()
+        return res.status(200).json({ chats: user.chats })
+        //get latest response
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Something went wrong" })
+    }
+
+}
--- backend/src/utils/validators.ts
+++ backend/src/utils/validators.ts
@@ -24,3 +24,8 @@ export const signupValidator = [
     body("name").notEmpty().withMessage("Name is required"),
     ...loginValidator,
 ];
+
+export const chatCompletionValidator = [
+    body("message").notEmpty().withMessage("Message is required"),
+    ...loginValidator,
+];
