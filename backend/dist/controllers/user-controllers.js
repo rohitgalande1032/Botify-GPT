6c377cbe5a7ca38a30eedce2dc82c7800dc30b2d
rohitgalande1032 <rohitg3234@gmail.com>
2024-10-06 10:36:20 +0530

    Add User registration functionality

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,4 +1,40 @@
-export const getAllUsers = () => {
-    // get all users from database
+import User from "../models/User.js";
+import { hash } from 'bcrypt';
+export const getAllUsers = async (req, res, next) => {
+    try {
+        // get all users from database
+        const users = await User.find();
+        return res.status(200).json({
+            message: "OK",
+            users
+        });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            message: "ERROR",
+            cause: error.message
+        });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    try {
+        //User Signup
+        const { name, email, password } = req.body;
+        //Check user already registered
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(401).send("User already registerd with this email !");
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        return res.status(200).json({ message: "OK", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "Error", cause: error.message });
+    }
+};
+export const userLogin = async (req, res, next) => {
 };
 //# sourceMappingURL=user-controllers.js.map
---
+++ backend/src/utils/validators.ts
@@ -0,0 +1,26 @@
+import { body, ValidationChain, validationResult } from 'express-validator'
+export const validate = (validations) => {
+    return async (req, res, next) => {
+        // Run all validations first
+        await Promise.all(validations.map(validation => validation.run(req)));
+
+        // Check for validation errors
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            return next(); // No errors, proceed to next middleware
+        }
+
+        // If there are errors, respond with a 422 and the error details
+        return res.status(422).json({ errors: errors.array() });
+    };
+};
+
+export const loginValidator = [
+    body("email").trim().isEmail().withMessage("Invalid email format"),
+    body("password").trim().isLength({ min: 6 }).withMessage("Password should be at least 6 characters long")
+];
+
+export const signupValidator = [
+    body("name").notEmpty().withMessage("Name is required"),
+    ...loginValidator,
+];
