2144857ab5b92542f1b82b101aee2cf8ffefede3
rohitgalande1032 <rohitg3234@gmail.com>
2024-10-07 02:27:17 +0530

    Complete authentication

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken"
+import { COOKIE_NAME } from "./constants.js"
 
 export const createToken = (id: string, email: string, expiresIn: string) => {
     const payload = { id, email }
@@ -6,4 +7,24 @@ export const createToken = (id: string, email: string, expiresIn: string) => {
         expiresIn,
     })
     return token
+}
+
+export const verifyToken = async (req, res, next) => {
+    const token = req.signedCookies[`${COOKIE_NAME}`]
+    //console.log(token)
+    if(!token || token.trim() === ""){
+        return res.status(401).json({message: "Token not received"})
+    }
+    return new Promise<void>((resolve, reject) => {
+        return jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
+            if(err) {
+                reject(err.message)
+                return res.status(401).json({message: "Token Expired"})
+            } else {
+                resolve()
+                res.locals.jwtData = success
+                return next()
+            }
+        })
+    })
 }
